997d0916580e84873ad4e9190c10e4b6ccb89381
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-06-11 08:56:38 +0300

    luotu mongodb

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://blogcollection:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://blogcollection:${password}@cluster0.a5qfl.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
