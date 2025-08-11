2bb976aad972134178f91f5f5cb8bc31f505abc4
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-08-11 13:39:54 +0300

    testien tekoa

--- mongo.js
+++ mongo.js
@@ -6,7 +6,7 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2]
-const url = `mongodb+srv://blogcollection:${password}@cluster0.y9hmhv7.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://blogcollection:${password}@cluster0.y9hmhv7.mongodb.net/testBlogApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
---
+++ tests/blog_api.test.js
@@ -0,0 +1,51 @@
+const assert = require("node:assert")
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require("../models/note")
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        author: "Pekkis",
+        title: "AI",
+        url: "www.ai.com",
+    },
+    {
+        author: "Jakkis",
+        title: "Food",
+        url: "www.food.com"
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+test("blogs are returned as json", async () => {
+    await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
+})
+
+test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs")
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test("a specifig blog is within the returned blogs", async () => {
+    const response = await api.get("/api/blogs")
+
+    const contents = response.body.map(e => e.content)
+    assert.strictEqual(contents.includes("AI"))
+})
+after(async () => {
+    await mongoose.connection.close()
+})
